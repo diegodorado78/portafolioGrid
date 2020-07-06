4546f45d850432d2b2ae530197f4b62159397f63
diego dorado <diegodorado78@gmail.com>
2020-07-06 11:38:34 -0500

    primer

---
+++ cv_grid.js
@@ -0,0 +1,28 @@
+var btn_mas= document.querySelector(".boton-mas");
+var span=document.querySelector("#span-mas");
+btn_mas.textContent="Leer más";//textContent para agregar texto
+
+btn_mas.addEventListener("click",function(){
+ if (span.style.display==="none"){//element.Style para cambiar estilos
+     span.style.display= "block";
+     btn_mas.textContent="Leer menos";
+ }else{
+    span.style.display="none";
+    btn_mas.textContent="Leer más";
+ }
+});
+
+function fibonacci(n) {
+    if (n <= 1) {
+      return n;
+    }
+     a = 0;
+     b = 1;
+    for (i = 2; i <= n; i++) {
+      temp = a;
+      a = b;
+      b += temp;
+    }
+    return b;
+  }
+    console.log(fibonacci(12));
